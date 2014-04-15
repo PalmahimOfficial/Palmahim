90be1bf44f56722a79622bc3b6888e9f5144dbf4
Gil Nisan <gilnis2@hotmail.com>
2014-04-16 00:56:33 +0300

    Many many things...

--- gen/com/IAF/Palmahim/R.java
+++ gen/com/IAF/Palmahim/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
+        public static final int MainActivityButtonSize=0x7f040002;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Customize dimensions originally defined in res/values/dimens.xml (such as
@@ -19,14 +20,16 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
-        public static final int helloworldsize=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int base_history_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int AboutButton=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int BaseHistoryButton=0x7f080000;
+        public static final int InformationButton=0x7f080001;
+        public static final int NoalNineButton=0x7f080002;
+        public static final int action_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,7 +40,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
@@ -66,5 +68,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int FullscreenTheme=0x7f060002;
     }
 }
--- src/com/IAF/Palmahim/MainActivity.java
+++ src/com/IAF/Palmahim/MainActivity.java
@@ -3,12 +3,20 @@ package com.IAF.Palmahim;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_main);
     }
 
